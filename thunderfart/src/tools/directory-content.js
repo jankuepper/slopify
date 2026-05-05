0fa14e4f0d6bfb54bfe9524f2910361bd8ed2356
Jan <janluca0000046@gmail.com>
2026-05-06 00:43:29 +0200

    debug

--- thunderfart/src/tools/directory-content.js
+++ thunderfart/src/tools/directory-content.js
@@ -1,6 +1,6 @@
 import { readdirSync } from "node:fs"
 export function cd() {
-  const dir = readdirSync(".", { encoding: 'utf-8', withFileTypes: true })
+  const dir = readdirSync("/mnt/Movies", { encoding: 'utf-8', withFileTypes: true })
   const result = dir.map((d) => d.name)
   const response = {
     content: [
