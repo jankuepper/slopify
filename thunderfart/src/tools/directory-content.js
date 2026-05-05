cc9d33edeac7d76f59ede7dd378bd37cf04ec98b
Jan <janluca0000046@gmail.com>
2026-05-06 00:10:35 +0200

    debug

--- thunderfart/src/tools/directory-content.js
+++ thunderfart/src/tools/directory-content.js
@@ -3,5 +3,5 @@ export function cd() {
   const dir = readdirSync(".", { encoding: 'utf-8', withFileTypes: true })
   const result = dir.map((d) => d.name)
   console.log(result)
-  return { "diretory-content": result }
+  return { directoryContent: result }
 }
--- thunderfart/src/tools/index.js
+++ thunderfart/src/tools/index.js
@@ -16,7 +16,7 @@ export function initTools(server) {
     'readdir',
     {
       description: 'list content of directory',
-      outputSchema: z.object({ "directory-content": z.array(z.string()) })
+      outputSchema: z.object({ directoryContent: z.array(z.string()) })
     },
     () => cd()
   )
